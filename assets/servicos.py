db9d652f6d8deda474f1f8af6e12c833634624b0
Mps <melissaguimaraes442@gmail.com>
2024-10-01 20:45:14 -0300

    arquivo serviços

---
+++ assets/servicos.py
@@ -0,0 +1,7 @@
+from assets.oferecimento import Oferecimento
+
+class servicos(oferecimento):
+
+    def __init__ (self,nome, descricao, fotos e valor):
+
+    __super__().__init__(nome, descricao, fotos e valor)
