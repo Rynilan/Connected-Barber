d518ba35ce2328f499bffc6a858fac36a53bd6e7
Crystian <crystiansantos6639@gmail.com>
2024-10-01 21:29:32 -0300

    Criação da classe barbearia
    eu criei as bases das funções dentro da classe barbearia

---
+++ assets/Class_Barbearia.py
@@ -0,0 +1,28 @@
+class Barbearia (Adm,Funcionario,Endereco,Produtos):
+
+    def __init__(self.nome):
+        self.nome = nome
+
+    def Criar_barbearia (self,nome,Adm,Funcionarios):
+        #será necessário fazer o login para criar a barbearia,se for um cliente não será possível criar.
+        #caso for um adm ele poderá criar uma barbearia
+        
+
+    def Nome_babebaria(Adm):
+        Nome_babebaria
+        #será possível apenas o adm criar o nome da barbearia.
+
+    def Mudar_nome_babebaria(Nome_barbearia):
+        #será possível apenas o adm mudar o nome da barbearia.
+
+    def Excluir_barbearia(Adm):
+        #será possível apenas o adm excluir a barbearia.
+
+    def Produtos_barbearia(Adm,funcionarios,Produtos):
+        #tanto o adm quanto o funciomário poderão adicionar os produtos na barbearia.
+   
+    def Endereco_babearia (Adm,Endereco):
+        #apenas o adm poderá cadastrar o endereço da barbearia.
+
+    def Mudar_endereco_babearia (Adm,Endereco):
+         #apenas o adm poderá mudar o endereço da basbearia.
