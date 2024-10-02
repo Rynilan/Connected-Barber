a95b45d8f4731ab532cfc88fe0e766d33f3e5949
Mps <melissaguimaraes442@gmail.com>
2024-10-01 21:11:17 -0300

    arquivo serviços (#1)
    * arquivo serviços
    
    * code: ajustada a identação e a herança (__super__())
    
    ---------
    
    Co-authored-by: Rynilan <ilanvitorsantos@gmail.com>

---
+++ assets/servicos.py
@@ -0,0 +1,6 @@
+from assets.oferecimento import Oferecimento
+
+class servicos(Oferecimento):
+
+    def __init__ (self,nome, descricao, fotos, valor):
+        super().__init__(nome, descricao, fotos, valor)
