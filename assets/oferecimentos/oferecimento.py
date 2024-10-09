9cf0646615f8df7801aa2229ae2b9ac8b08b0163
Rynilan <ilanvitorsantos@gmail.com>
2024-10-09 16:47:01 -0300

    code: adicionados métodos necessários para a classe oferecimento.
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- assets/oferecimentos/oferecimento.py
+++ assets/oferecimentos/oferecimento.py
@@ -6,11 +6,13 @@ class Oferecimento:
                  nome: str,
                  descricao: str,
                  fotos: tuple[path],
-                 valor: float) -> None:
+                 valor: float,
+                 barbearia: object) -> None:
         self._nome = nome
         self._descricao = descricao
         self._fotos = fotos
         self._valor = valor
+        self._barbearia = barbearia
 
     def get_fotos(self: object) -> tuple[path]:
         return self._fotos
@@ -21,5 +23,27 @@ class Oferecimento:
     def get_valor(self: object) -> float:
         return self._valor
 
-    def calcular_preco(self: object) -> int:
-        pass
+    def get_barbearia(self: object) -> object:
+        return self._barbearia
+
+    def set_valor(self: object, operador: object, valor: float) -> None:
+        if operador.compare_barbearia(self._barbearia):
+            self._valor = valor
+
+    def set_descricao(self: object, operador: object, descricao: str) -> None:
+        if operador.compare_barbearia(self._barbearia):
+            self._descricao = descricao
+
+    def adicionar_foto(self: object, operador: object, foto: str) -> None:
+        if (operador.compare_barbearia(self._barbearia) and
+                operador.get_nivel() == 'adm'):
+            self._fotos = list(self._fotos)
+            self._fotos.append(foto)
+            self._fotos = tuple(self._fotos)
+
+    def remover_foto(self: object, operador: object, foto: str) -> None:
+        if (operador.compare_barbearia(self._barbearia) and
+                operador.get_nivel() == 'adm'):
+            self._fotos = list(self._fotos)
+            self._fotos.pop(self._fotos.index(foto))
+            self._foto = tuple(self._fotos)
