3cb7a54d8f3268eeaec5872e8ec01a09c37a4165
Mps <melissaguimaraes442@gmail.com>
2024-10-15 20:46:23 -0300

    mais um arquivo chato

---
+++ model/create.py
@@ -0,0 +1,17 @@
+from mysql.connector import connect
+
+def create(id_portfolio,endereco):
+    # Srtª Paiva
+    banco = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="",
+      database="Connected_Barber"
+    )
+    cursor = banco.cursor()
+    cursor.execute("insert into tabela_fotos(id,endereco,fk_portfolio)" +
+                   "values (default, '{}', {});".format(endereco,id_portfolio))
+    cursor.close()
+    banco.close()
+    
+
