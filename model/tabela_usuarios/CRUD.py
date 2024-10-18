e3846727e652dcea90788ddeda6b4c8da352cc4e
Rynilan <ilanvitorsantos@gmail.com>
2024-10-18 00:24:51 -0300

    code: atualizado a forma de organização da tabela de usuários, adicionados as funções restantes.
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- model/tabela_usuarios/CRUD.py
+++ model/tabela_usuarios/CRUD.py
@@ -37,3 +37,40 @@ def insert(val):
     mydb.commit()
     mycursor.close()
     mydb.close()
+
+
+def select(email):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="Connected_Barber"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM tabela_usuarios where email="+email)
+    resultados = mycursor.fetchall()
+    mycursor.close()
+    mydb.close()
+    return resultados
+
+
+def update(campos, valores, email):
+    mydb = mysql.connector.connect(
+        host="root",
+        user="connected_barber",
+        password="",
+        database="tabela_barbearias"
+    )
+    mycursor = mydb.cursor()
+    formatada = ""
+    for indice in range(0, len(campos)):
+        formatada += campos[indice] + " = " + valores[indice] + ", "
+    formatada.removesuffix(", ")
+    sql = "UPDATE tabela_usuarios SET {} where email= '{}'".format(
+        formatada, email)
+    mycursor.execute(sql)
+    mydb.commit()
+    mycursor.close()
+    mydb.close()
