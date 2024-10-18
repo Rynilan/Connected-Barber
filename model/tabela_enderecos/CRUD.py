934a28de894dbd6abfcc9dd2fb7ddbf8bfaaee76
Rynilan <ilanvitorsantos@gmail.com>
2024-10-18 00:17:49 -0300

    code: unido arquivo de conexão com o de crud da tabela endereços.
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- model/tabela_enderecos/CRUD.py
+++ model/tabela_enderecos/CRUD.py
@@ -1,4 +1,20 @@
-from connect import conectar
+import mysql.connector
+
+
+def conectar():
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="Connect_Barber"
+        )
+
+        mycursor = mydb.cursor()
+        print("Conectado no banco com sucesso!")
+        return mydb, mycursor
+    except:
+        print("Conexao falhou!")
 
 
 def create(self, pais, estado, cidade, cep, complemento):
