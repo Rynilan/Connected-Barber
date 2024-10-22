35db81f54c0d8a6efc28c200489cd76e613d6753
Crystian <crystiansantos6639@gmail.com>
2024-10-22 20:26:10 -0300

    tb_servicos
    foram feitas os crud para a tabela serviços

---
+++ crud.py
@@ -0,0 +1,48 @@
+from conn import *
+
+def create(id, nome, preco):
+        
+        sql = str("INSERT INTO tabela_enderecos (fk_id,fk_nome,fk_preco," ) 
+        VALUES ( %s, %s, %s)"
+        val = (id, nome, preco,)
+        mycursor.execute(sql, val)
+        mydb.commit()
+        mycursor.close()
+        mydb.close()
+
+
+def select(identificador):
+
+        mycursor.execute("SELECT * FROM tabela_enderecos where id = {}".format(
+            identificador
+        ))
+        mycursor.close()
+        mydb.close()
+        return mycursor.fetchall()
+
+
+
+def delete(id):
+
+        sql = "DELETE FROM tabela_enderecos WHERE id = %s"
+        mycursor.execute(sql, (id,))
+        mydb.commit()
+        mycursor.close()
+        mydb.close()
+
+
+def update(identificador, campos, valores):
+
+    if len(campos) != len(valores) and campos and valores:
+        print("erro, tamanho dos campos difere dos valores")
+        return
+    parametros_sql = ''
+    for indice in range(0, len(campos)):
+        parametros_sql += '{} = {}, '.format(campos[indice],
+                                             valores[indice])
+    parametros_sql.removesuffix(', ')
+    mycursor.execute('update tabela_enderecos set {} where id = {};'.format(
+        parametros_sql, identificador
+    ))
+    mycursor.close()
+    mydb.close()
