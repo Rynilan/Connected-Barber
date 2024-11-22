2db40f7c30443ffc7b43cc929271ccf654c99cf1
Paulo Gabriel da luz <paulogp4319@gmail.com>
2024-11-22 20:47:17 -0300

    formatador

---
+++ formatador.py
@@ -0,0 +1,12 @@
+def formatar_nome(nome):
+    return nome.capitalize()
+nome = ""
+nome_formatado = formatar_nome(nome)
+print(nome_formatado)
+
+def formatar_nome(nome):
+    return nome.CAPITALIZE()
+nome = ""
+nome_formatado = formatar_nome(nome)
+print(nome_formatado)
+
