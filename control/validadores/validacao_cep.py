0b4528bb91f5de19f8f760de92c8d8918ac507b7
Rynilan <ilanvitorsantos@gmail.com>
2024-11-30 22:04:44 -0300

    fix: ajustada a função de validação de CEP.'
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- control/validadores/validacao_cep.py
+++ control/validadores/validacao_cep.py
@@ -4,6 +4,7 @@ import re
 class BuscaEndereco:
     def __init__(self, cep):
         cep = str(cep)
+        return self.cep_eh_Valido(cep)
         if self.cep_eh_Valido(cep):
             self.cep = cep
         else:
@@ -13,8 +14,12 @@ class BuscaEndereco:
         if len(cep) == 8:
             padrao_cep = re.compile(r'(\d){5}(\d){3}')
 
-            match = padrao_cep.match(cep)
-            match
+            padrao_cep.match(cep)
+
+            for elemento in cep:
+                if elemento not in '1234567890':
+                    return False
+
             return True
         else:
             return False
